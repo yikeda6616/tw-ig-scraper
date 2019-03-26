591e07f8543b991fbd181a9a11622c934c5bdafe
Yasushi Ikeda <yikeda6616@gmail.com>
2019-03-26 07:58:56 -0700

    add twitter follower function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { getHTML } from './lib/scraper';
+import { getHTML, getTwitterFollowers } from './lib/scraper';
 
 console.log(getHTML());
+
+async function go() {
+  const html = await getHTML('https://twitter.com/rushtheband');
+  const twCount = await getTwitterFollowers(html);
+  console.log(`You have ${twCount} followers`);
+}
+
+go();

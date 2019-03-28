84e805808db544feea005abec61868a38f3f2b78
Yasushi Ikeda <yikeda6616@gmail.com>
2019-03-28 01:06:00 -0700

    TypeError[WIP]

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import { getHTML, getTwitterFollowers } from './lib/scraper';
+import {
+  getHTML,
+  getTwitterFollowers,
+  getInstagramFollowers
+} from './lib/scraper';
 
 console.log(getHTML());
 
 async function go() {
-  const html = await getHTML('https://twitter.com/rushtheband');
-  const twCount = await getTwitterFollowers(html);
-  console.log(`You have ${twCount} followers`);
+  // const igPromise = getHTML('https://instagram.com/instagram');
+  const twPromise = getHTML('https://twitter.com/Twitter');
+  const twHTML = await twPromise; // temporary
+  // const [igHTML, twHTML] = await Promise.all([igPromise, twPromise]);
+  // const igCount = await getInstagramFollowers(igHTML);
+  const twCount = await getTwitterFollowers(twHTML);
+  console.log(
+    `You have ${twCount} twitter followers `
+    // and ${igCount} instagram followers`
+  );
 }
 
 go();

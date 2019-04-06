e5dea49681c88960b7b984f9be064fc41e74aece
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-05 17:52:55 -0700

    add cron job

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getInstagramCount, getTwitterCount } from './lib/scraper';
-import db from './lib/db';
+import './lib/cron';
 
 const app = express();
 
@@ -12,27 +12,9 @@ app.get('/scrape', async (req, res, next) => {
     getTwitterCount()
   ]);
 
-  console.log(twCount, igCount);
-
-  db.get('twitter')
-    .push({
-      date: Date.now(),
-      count: twCount
-    })
-    .write();
-  db.get('instagram')
-    .push({
-      date: Date.now(),
-      count: igCount
-    })
-    .write();
   res.json({ igCount, twCount });
 });
 
 app.listen(3000, () => {
-  console.log('Example App Running on PORT 3000');
+  console.log('App Running on PORT 3000');
 });
-
-// 1. Make an Express Server
-// 2. Save data to db
-// 3. Setup a cron job
